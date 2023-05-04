6a0a7d761125ee5ec1c02be061e59c510fa20847
Odbilig <odko@bbt-2.local>
2023-05-04 10:53:25 +0800

    galzuu

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,12 @@
 import Head from "next/head";
-import React from "react";
-import { useRouter} from "next/router";
 import Image from "next/image";
+import { userAgentFromString } from "next/server";
+import Header from "./components/header";
 
-function Header() {
-  const router = useRouter();
-  return(
-    <div className="fixed left-0 py-4 text-white top-0 bg-stone-800 w-screen flex gap-40 px-8">
-      <Image src="https://upload.wikimedia.org/wikipedia/commons/thumb/f/f1/Pornhub-logo.svg/2560px-Pornhub-logo.svg.png"  width={100} height={0}/>
-      <span className="text-white" onClick={() => router.push("/")}>Төрөл</span>
+export default function Home(){
+  return (
+    <div className="w-screen h-screen pt-32 bg-black  items-center jutsify-center text-white grid grid-cols-4">
+      <Header />
     </div>
   );
-}
-
-export default Header;
+};
