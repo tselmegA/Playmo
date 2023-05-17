44f8bec8ae4fcdc6f85ff85139c5e57dff7605ab
Odbilig <odko@bbt-2.local>
2023-05-17 10:37:05 +0800

    YUMA HII LALRUD

--- pages/components/header.js
+++ pages/components/header.js
@@ -1,42 +1,155 @@
 import Head from "next/head";
 import React from "react";
-import { useRouter} from "next/router";
+import { useRouter } from "next/router";
 import Image from "next/image";
 import { useState } from "react";
 
-  export default function Header() {
-    const [opacity, setOpacity] = useState(false);
-    const changeOpacity = () => {
-      if (typeof window !== "undefined") {
-        if (window.scrollY >= 8) setOpacity(true);
-        else setOpacity(false);
-      }
-    };
-    if (typeof window !== "undefined") {
-      window.addEventListener("scroll", changeOpacity);
-  };
+export default function Header() {
   const router = useRouter();
-  return(
-    <div className= {opacity ? "h-16 fixed z-50 left-0 text-white top-0 bg-[#2B2B2B] w-screen flex items-center gap-20 px-8 duration-700" : " h-16 fixed left-0 text-white top-0 w-screen z-50 flex items-center gap-20 px-8 duration-700"}>
-      <div className="w-64 h-10 bg-contain bg-no-repeat bg-[url('/Logo.png')]"></div>
-      <button className="text-white text-xl font-semibold flex flex-row gap-2 " onClick={() => router.push("/")}>Төрөл
-      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="mt-px w-6 h-6">
-  <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 8.25l-7.5 7.5-7.5-7.5" />
-</svg>
- </button>
-      
-      <div className="w-full flex justify-end">
-        <button>
-          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-8 h-8 mr-20">
-          <path strokeLinecap="round" strokeLinejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" />
-          </svg>
+  const [opacity, setOpacity] = useState(false);
+  const [search, setSearch] = useState(false);
+  const [login, setLogin] = useState(false);
+  const haih = () => {
+    if (search == false) {
+      setSearch(true);
+    } else {
+      setSearch(false);
+    }
+  };
+  const nevtreh = () => {
+    if (login == false) {
+      setLogin(true);
+    } else {
+      setLogin(false);
+    }
+  };
+  const changeOpacity = () => {
+    if (typeof window !== "undefined") {
+      if (window.scrollY >= 8) setOpacity(true);
+      else setOpacity(false);
+    }
+  };
+  if (typeof window !== "undefined") {
+    window.addEventListener("scroll", changeOpacity);
+  }
+  return (
+    <div
+      className={
+        opacity
+          ? "h-16 fixed z-50 left-0 text-white top-0 bg-[#2B2B2B] w-screen flex items-center gap-20 px-8 duration-700"
+          : " h-16 fixed left-0 text-white top-0 w-screen z-50 flex items-center gap-20 px-8 duration-700"
+      }
+    >
+      <button className="w-64 h-10 bg-contain bg-no-repeat bg-[url('/Logo.png')]"></button>
+      <button
+        className="text-white text-lg font-semibold flex flex-row gap-2 "
+        onClick={() => router.push("/")}
+      >
+        Төрөл
+        <TurulIcon />
+      </button>
+
+      <div className="w-full flex justify-end  items-center">
+        <input
+          className={
+            search
+              ? "  flex items-center rounded p-2 w-[185px] h-12 bg-[#2B2B2B] mr-5 outline-none"
+              : "w-32 h-24 bg-[#2B2B2B] hidden "
+          }
+          placeholder="Хайх..."
+        />
+        <button onClick={haih}>
+          <SearchIcon />
         </button>
-        <button>
-          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-8 h-8 mr-6">
-          <path strokeLinecap="round" strokeLinejoin="round" d="M9 12.75l3 3m0 0l3-3m-3 3v-7.5M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
-          </svg>
-        </button>
+        <div className="relative">
+          <button onClick={nevtreh}>
+            <SumIcon />
+          </button>
+          <button
+            onClick={() => router.push("/login")}
+            className={
+              login
+                ? "absolute right-0 top-12 flex  justify-center border items-center rounded p-2 w-[140px] h-[55px] bg-[#353535] mr-5 gap-3 border-opacity-10"
+                : "w-32 h-24 bg-[#353535] hidden "
+            }
+          >
+            <NevtrehIcon />
+            Нэвтрэх
+          </button>
+        </div>
       </div>
     </div>
   );
 }
+
+const TurulIcon = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      strokeWidth={1.5}
+      stroke="currentColor"
+      className="mt-px w-6 h-6"
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M19.5 8.25l-7.5 7.5-7.5-7.5"
+      />
+    </svg>
+  );
+};
+
+const SearchIcon = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      strokeWidth={1.5}
+      stroke="currentColor"
+      className="w-8 h-8 mr-20"
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z"
+      />
+    </svg>
+  );
+};
+
+const SumIcon = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      strokeWidth={1.5}
+      stroke="currentColor"
+      className="w-8 h-8 mr-6"
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M9 12.75l3 3m0 0l3-3m-3 3v-7.5M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+      />
+    </svg>
+  );
+};
+
+const NevtrehIcon = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="#ffffff"
+      viewBox="0 0 32 32"
+      strokeWidth={1 / 2}
+      stroke="currentColor"
+      className="w-4 h-4 "
+    >
+      <path d="M16.642 20.669c-0.391 0.39-0.391 1.023-0 1.414 0.195 0.195 0.451 0.293 0.707 0.293s0.512-0.098 0.707-0.293l5.907-6.063-5.907-6.063c-0.39-0.39-1.023-0.39-1.414 0s-0.391 1.024 0 1.414l3.617 3.617h-19.264c-0.552 0-1 0.448-1 1s0.448 1 1 1h19.326zM30.005 0h-18c-1.105 0-2.001 0.895-2.001 2v9h2.014v-7.78c0-0.668 0.542-1.21 1.21-1.21h15.522c0.669 0 1.21 0.542 1.21 1.21l0.032 25.572c0 0.668-0.541 1.21-1.21 1.21h-15.553c-0.668 0-1.21-0.542-1.21-1.21v-7.824l-2.014 0.003v9.030c0 1.105 0.896 2 2.001 2h18c1.105 0 2-0.895 2-2v-28c-0.001-1.105-0.896-2-2-2z"></path>
+    </svg>
+  );
+};
--- pages/index.js
+++ pages/index.js
@@ -2,15 +2,82 @@ import Head from "next/head";
 import Image from "next/image";
 import { userAgentFromString } from "next/server";
 import Header from "./components/header";
+import { useRouter } from "next/router";
 
-export default function Home(){
+export default function Home() {
+  const router = useRouter();
   return (
-    <div className="w-screen h-[200vh]  bg-black  items-start jutsify-center text-white grid grid-cols-4">
+    <div className="w-screen h-[200vh]  bg-black  items-start text-white ">
       <Header />
-      <div className="relative  w-screen h-[80vh] bg-contain flex bg-no-repeat bg-[url(https://api.playmo.mn/storage/images/media/banner/Q2tkEXSJkAjLB5ycvodyeTdR9aoKsYJU482BezXk.jpg)] bg-opacity-50">
-        <div className="absolute z-0 left-0 top-0 w-1/2 h-[80vh] bg-gradient-to-r from-black to-transparent"></div>
+      <div className="  w-screen h-[66vh] bg-cover flex bg-center bg-[url(https://api.playmo.mn/storage/images/media/banner/fa2XZtqZmgovgw9ppaLD7A17FUvvJFCNRzf5r5LK.jpg)]">
+        <div className="absolute z-0 left-0 top-0 w-1/3 h-[66vh] bg-gradient-to-r from-black to-transparent"></div>
         <div className="w-screen z-0 h-[20vh] bg-gradient-to-b	absolute top-0  from-black to-transparent"></div>
+        <div className="flex  z-40 justify-center flex-col  items-center  ">
+          <img
+            className="w-full"
+            src="https://api.playmo.mn/storage/images/media/logo/md/fudaddq3LwLjgpRI90bn1H0aeCBX7WF0ciW8DDz8.png"
+          />
+          <div className="flex gap-10  items-center  ">
+            <button
+              onClick={() => router.push("/login")}
+              className="w-[16vh] h-[6vh] rounded flex justify-center items-center font-medium text-lg  bg-[#29b75a]"
+            >
+              Шууд үзэх
+            </button>
+            <button className=" w-[12vh] h-[8vh] flex flex-col items-center justify-center hover:bg-[#ffffff] hover:bg-opacity-10">
+              <ListIcon />
+              <div className="text-xs">Жагсаалт</div>
+            </button>
+            <button className=" w-[10vh] h-[8vh] flex flex-col justify-center items-center hover:bg-[#ffffff] hover:bg-opacity-10 ">
+              <InfoIcon />
+              <div className="text-xs ">Тойм</div>
+            </button>
+          </div>
+        </div>
       </div>
     </div>
+  );
+}
+
+const ListIcon = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      strokeWidth={1.5}
+      stroke="currentColor"
+      className="w-12 h-12  "
+    >
+      <path
+        d="M17 19V11M3 11H13M3 7H13M3 15H9M13 15H21"
+        stroke="white"
+        stroke-width="1"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+    </svg>
+  );
+};
+
+const InfoIcon = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      strokeWidth={1}
+      stroke="currentColor"
+      className="w-9 h-9 "
+    >
+      <path
+        id="Vector"
+        d="M12 11V16M12 21C7.02944 21 3 16.9706 3 12C3 7.02944 7.02944 3 12 3C16.9706 3 21 7.02944 21 12C21 16.9706 16.9706 21 12 21ZM12.0498 8V8.1L11.9502 8.1002V8H12.0498Z"
+        stroke="white"
+        stroke-width="1"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+    </svg>
   );
 };
