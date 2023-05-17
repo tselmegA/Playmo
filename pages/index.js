6200fffacc0607125f23e911e7f09f69f2465adb
yalalt <yalalt@yalalts-MacBook-Pro.local>
2023-05-17 15:22:57 +0800

    KK

--- pages/index.js
+++ pages/index.js
@@ -7,7 +7,7 @@ export default function Home(){
   return (
     <div className="w-screen h-[200vh]  bg-black  items-start jutsify-center text-white grid grid-cols-4">
       <Header />
-      <div className=" w-screen h-screen bg-cover bg-no-repeat bg-[url(https://i.ytimg.com/vi/_e9y729xeck/maxresdefault.jpg)]"></div>
+      <div className=" w-screen h-[800px] bg-cover bg-no-repeat bg-[url(https://i.ytimg.com/vi/_e9y729xeck/maxresdefault.jpg)]"></div>
     </div>
   );
 };
